4a8e7dfa1531def2f1abf03ab4e2ee204ac8b822
dattranuit <18520590@gm.uit.edu.vn>
2020-12-08 12:42:32 +0700

    Add log in

--- QuanLyCuaHangThoiTrang/Controllers/HomeController.cs
+++ QuanLyCuaHangThoiTrang/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using QuanLyCuaHangThoiTrang.Model;
 
 namespace QuanLyCuaHangThoiTrang.Controllers
@@ -10,6 +11,7 @@ namespace QuanLyCuaHangThoiTrang.Controllers
     public class HomeController : Controller
     {
         QuanLyCuaHangThoiTrangDbContext db = new QuanLyCuaHangThoiTrangDbContext();
+        public string HoTen = "";
         public ActionResult Index()
         {
             
@@ -35,6 +37,20 @@ namespace QuanLyCuaHangThoiTrang.Controllers
             ViewBag.MenWears = db.LoaiHangHoas.Where(lhh => lhh.GioiTinh == "Nam").ToList();
             ViewBag.WomenWears = db.LoaiHangHoas.Where(lhh => lhh.GioiTinh == "Nữ").ToList();
             return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection form)
+        {
+            string username = form["username"].ToString();
+            string password = form["password"].ToString();
+
+            var nguoiDung = db.NguoiDungs.SingleOrDefault(n => n.UserName == username && n.PassWord == password);
+            if(nguoiDung != null)
+            {
+                Session["Account"] = nguoiDung;
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
