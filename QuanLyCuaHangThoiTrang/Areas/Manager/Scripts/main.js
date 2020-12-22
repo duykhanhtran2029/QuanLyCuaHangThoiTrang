f908fbef63629431656cc7574c776e14a2f8a3bf
Khánh Trần Duy <18520075@gm.uit.edu.vn>
2020-12-22 23:46:46 +0700

    all done x2

--- QuanLyCuaHangThoiTrang/Areas/Manager/Scripts/main.js
+++ QuanLyCuaHangThoiTrang/Areas/Manager/Scripts/main.js
@@ -13,15 +13,8 @@
 
 $(document).ajaxComplete(function () {
     $(".btnDel").attr('data-toggle', 'modal');
-    $(".btnDel").attr('data-target', '#Modal');
-
-    $('.datetimepicker').datepicker(
-        {
-            dateFormat: 'mm/dd/yyyy',
-            changeMonth: true,
-            changeYear: true,
-            yearRange: "-60:+0" 
-        }); 
+    $(".btnDel").attr('data-target', '#Modal'); 
+    $('.datetimepicker').datepicker();
 });
 
 function LoadDatatable() {
